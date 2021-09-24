25751f3ea5f6380030ffd59a64c3cc4ae59ea8d7
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-09-24 22:14:38 +0300

    Implement actors controller logic

---
+++ MoviesAPI/Controllers/ActorsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.DTOs;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/actors")]
+    [ApiController]
+    public class ActorsController : ControllerBase
+    {
+        private readonly MoviesDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ActorsController(
+            MoviesDbContext context,
+            IMapper mapper)
+        {
+            this._context = context;
+            this._mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ActorDto>>> Get()
+        {
+            var actors = await this._context
+                .Actors
+                .ToListAsync();
+
+            return this._mapper.Map<List<ActorDto>>(actors);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ActorDto>> Get(int id)
+        {
+            var actor = await this._context
+                .Actors
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            return this._mapper.Map<ActorDto>(actor);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ActorCreationDto actorCreationDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] ActorCreationDto actorCreationDto)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var actor = await this._context
+                .Actors
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            this._context.Remove(actor);
+            await this._context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
--- MoviesAPI/DTOs/ActorDto.cs
+++ MoviesAPI/DTOs/ActorDto.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MoviesAPI.DTOs
 {
     public class ActorDto
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Biography { get; set; }
+
+        public string Picture { get; set; }
     }
 }
